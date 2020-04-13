07f0820bf5c02364c393b74320237f93bcddc450
Manuel Vera <manuelv@lagash.com>
2020-04-13 15:50:48 -0400

    solucion ejercicio

--- src/com/javaTest/Helper.java
+++ src/com/javaTest/Helper.java
@@ -1,4 +1,30 @@
 package com.javaTest;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Helper {
+
+    static public Reader openReader(String fileName) throws IOException {
+        return Files.newBufferedReader(Paths.get(fileName));
+    }
+
+    static public Writer openWriter(String fileName) throws IOException {
+        return Files.newBufferedWriter(Paths.get(fileName));
+    }
+
+
+    static public ArrayList<Character> sortAscending(ArrayList<Character> arrayList) {
+        Collections.sort(arrayList);
+        return arrayList;
+    }
+
+    public static boolean isVocal(char c) {
+        return "AEIOUaeiou".indexOf(c) != -1;
+    }
 }
--- src/com/javaTest/Main.java
+++ src/com/javaTest/Main.java
@@ -1,7 +1,36 @@
 package com.javaTest;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.ArrayList;
+
+import static com.javaTest.Helper.*;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("hello test");
+
+        ArrayList<Character> pivotArray = new ArrayList();
+        try (Reader reader = openReader("file.txt");
+             Writer writer = openWriter("ManuelVera.txt"))  {
+            int length ;
+            while((length = reader.read()) != -1) {
+                char datachar = (char) length;
+                if (!Helper.isVocal(datachar)) {
+                    pivotArray.add(datachar);
+                    reader.read();
+                }
+            }
+
+            for (Character i : sortAscending(pivotArray)) {
+                writer.write(i.toString(), 0, 1);
+            }
+
+
+        } catch(IOException e) {
+            System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
+        }
     }
+
 }
