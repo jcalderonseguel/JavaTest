9b119042b0102b32dbad54d0d9af1cf17d72494a
Franco Arenas <francoa@lagash.com>
2020-04-13 11:51:29 -0400

    usando helper

--- src/com/javaTest/Helper.java
+++ src/com/javaTest/Helper.java
@@ -3,17 +3,34 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 
+import static java.lang.Character.toUpperCase;
+
 public class Helper {
 
-    public static void abrirArchivo(String archivo) {
+    public static void ordenarArchivo() {
         try {
-            Reader reader = new FileReader(archivo);
+            Reader reader = new FileReader("C:\\Git\\Lagash\\java\\JavaTest\\file.txt");
 
             int data = reader.read();
+            ArrayList<String> abecedario = new ArrayList<String>();
             while(data != -1){
+
                 char dataChar = (char) data;
                 data = reader.read();
+                if(toUpperCase(dataChar) != 'A' && toUpperCase(dataChar) != 'E' && toUpperCase(dataChar) != 'I' && toUpperCase(dataChar) != 'O' && toUpperCase(dataChar) != 'U')
+                    abecedario.add(String.valueOf(dataChar));
             }
+
+            Collections.sort(abecedario);
+
+            Writer writer = new FileWriter("C:\\Git\\Lagash\\java\\JavaTest\\Franco.txt");
+            for(String temp: abecedario){
+                writer.write(temp);
+            }
+            writer.flush();
+            writer.close();
+
+            System.out.println("COMO VAMOS");
 
         }catch (IOException e)
         {
