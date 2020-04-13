c7227698b941d209a06f03947fd65c160892f0e0
Aguayo Muñoz, Pedro <pedroam@lagash.com>
2020-04-13 11:25:56 -0400

    finish exercise

--- src/com/javaTest/Helper.java
+++ src/com/javaTest/Helper.java
@@ -1,4 +1,46 @@
 package com.javaTest;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Helper {
+
+    public static List<Character> getArrayFile(String path) throws IOException {
+
+        File file = new File(path);
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        String st, output = null;
+        List<Character> array = new ArrayList<Character>();
+        
+        while ((st = br.readLine()) != null) output = st;
+
+        for (char element: output.toCharArray()
+             ) {
+            array.add(element);
+        }
+
+        return array;
+    }
+
+    public static List<Character> removeVocal(List<Character> array) {
+
+        array.remove(Character.valueOf('A'));
+        array.remove(Character.valueOf('E'));
+        array.remove(Character.valueOf('I'));
+        array.remove(Character.valueOf('O'));
+        array.remove(Character.valueOf('U'));
+
+        return array;
+    }
+
+    public static void writeFile(String str, String filename) throws IOException {
+
+        FileWriter wrt = new FileWriter(filename);
+        wrt.write(str);
+        wrt.close();
+
+    }
+
 }
--- src/com/javaTest/Main.java
+++ src/com/javaTest/Main.java
@@ -1,7 +1,30 @@
 package com.javaTest;
 
+import java.io.*;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello test");
+
+        try {
+//
+            List<Character> list = Helper.getArrayFile("file.txt");
+            List<Character> list2 = Helper.removeVocal(list);
+            Collections.sort(list2);
+            String stringwithoutvocal = "";
+            for (char element: list2
+                 ) {
+                System.out.print(element);
+                stringwithoutvocal = stringwithoutvocal + element;
+            }
+
+            //write
+            Helper.writeFile(stringwithoutvocal,"pedro.txt");
+
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+
     }
 }
