8f6fb7422a7d2d96f633b6e5d435014e5d72783b
dolbyart <artfg@mail.ru>
2020-04-13 10:31:25 -0400

    orderstring

--- src/com/javaTest/Helper.java
+++ src/com/javaTest/Helper.java
@@ -1,4 +1,28 @@
 package com.javaTest;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Helper {
+    static public String readFromFile(String fileName) throws IOException {
+        return Files.readString(Paths.get(fileName), StandardCharsets.US_ASCII);
+    }
+
+    static public void writeToFile(String fileName, String output) throws FileNotFoundException {
+        PrintWriter out = new PrintWriter(fileName);
+        out.println(output);
+        out.close();
+    }
+
+    static public String sortString(String inputString){
+        char charArray[] = inputString.toCharArray();
+        Arrays.sort(charArray);
+        return new String(charArray);
+    }
 }
--- src/com/javaTest/Main.java
+++ src/com/javaTest/Main.java
@@ -1,7 +1,29 @@
 package com.javaTest;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Formatter;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello test");
+        readClearSortSaveString();
+    }
+
+    public static void readClearSortSaveString() {
+        try {
+            String content = Helper.readFromFile("file.txt");
+            System.out.println(String.format("String before: %s", content));
+            Helper.writeToFile("artour.txt",Helper.sortString(content.replaceAll("[AEIOUaeiou]", "")));
+            content = Helper.readFromFile("artour.txt");
+            System.out.println(String.format("String after: %s", content));
+
+        } catch (IOException e) {
+            System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
+        }
     }
 }
