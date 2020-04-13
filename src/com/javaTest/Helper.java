fa82d922455abb83c42312e7990dee4023b2825b
Franco Arenas <francoa@lagash.com>
2020-04-13 11:26:07 -0400

    push inicial

--- src/com/javaTest/Helper.java
+++ src/com/javaTest/Helper.java
@@ -1,4 +1,17 @@
 package com.javaTest;
 
+import java.io.FileReader;
+import java.io.Reader;
+
 public class Helper {
+
+   /* public static Reader abrirArchiv(String archivo) {
+        Reader reader = new FileReader("c:\\data\\myfile.txt");
+
+        int data = reader.read();
+        while(data != -1){
+            char dataChar = (char) data;
+            data = reader.read();
+        }
+    }*/
 }
--- src/com/javaTest/Main.java
+++ src/com/javaTest/Main.java
@@ -1,7 +1,43 @@
 package com.javaTest;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import static java.lang.Character.toUpperCase;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello test");
+        System.out.println("Bienvenido al ordenamiento de datos");
+        try
+        {
+
+            Reader reader = new FileReader("C:\\Git\\Lagash\\java\\JavaTest\\file.txt");
+
+            int data = reader.read();
+            ArrayList<String> abecedario = new ArrayList<String>();
+            while(data != -1){
+
+                char dataChar = (char) data;
+                data = reader.read();
+                if(toUpperCase(dataChar) != 'A' && toUpperCase(dataChar) != 'E' && toUpperCase(dataChar) != 'I' && toUpperCase(dataChar) != 'O' && toUpperCase(dataChar) != 'U')
+                abecedario.add(String.valueOf(dataChar));
+            }
+
+            Collections.sort(abecedario);
+
+            Writer writer = new FileWriter("C:\\Git\\Lagash\\java\\JavaTest\\Franco.txt");
+            for(String temp: abecedario){
+                writer.write(temp);
+            }
+            writer.flush();
+            writer.close();
+
+            System.out.println("COMO VAMOS");
+
+        }catch (IOException e)
+        {
+            System.out.println(e.getClass(). getSimpleName() + " - " + e.getMessage());
+        }
     }
 }
