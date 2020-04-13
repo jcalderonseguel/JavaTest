afda6076b8a872423b476e85285b417d15467c05
diegoc <diegoc@lagash.com>
2020-04-13 12:15:30 -0400

    ejercicioCollectionsFilesSortedVowels

--- src/com/javaTest/Helper.java
+++ src/com/javaTest/Helper.java
@@ -1,4 +1,49 @@
 package com.javaTest;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Helper {
+
+    public static ArrayList<String> readFileTxt() {
+        BufferedReader reader = null;
+        ArrayList<String> result = new ArrayList<>();
+        try{
+            reader = new BufferedReader(new FileReader("file.txt"));
+            while (reader.ready()) {
+                result.add(reader.readLine());
+            }
+        }catch( IOException e){
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                if (reader != null)
+                    reader.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return result;
+
+    }
+    public static ArrayList<String> sortCharacters(ArrayList<String> array) {
+        Collections.sort(array);
+        return array;
+    }
+    public  static void writeFileTxt (ArrayList<String> list) {
+        try{
+            FileWriter writer = new FileWriter("DiegoCalzadilla.txt");
+            StringBuilder sb = new StringBuilder();
+            for (String s : list) {
+                sb.append(s);
+            }
+            writer.write(sb.toString());
+            writer.close();
+            System.out.println("Successfully wrote to the file.");
+        }catch (IOException e){
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
 }
--- src/com/javaTest/Main.java
+++ src/com/javaTest/Main.java
@@ -1,7 +1,24 @@
 package com.javaTest;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello test");
+        System.out.println("hi dude!!");
+        ArrayList<String> result = new ArrayList<>();
+        ArrayList<String> sorted = new ArrayList<>();
+        String[] split;
+        String noVowels;
+
+        result = Helper.readFileTxt();
+
+        noVowels = result.get(0).replaceAll("[AEIOUaeiou]","");
+
+        split = noVowels.split("");
+        ArrayList<String> splitList = new ArrayList( Arrays.asList( split ) );//array de string a arrayList
+
+        sorted = Helper.sortCharacters(splitList);
+
+        Helper.writeFileTxt(sorted);
     }
 }
