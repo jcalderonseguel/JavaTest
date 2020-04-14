7905c23cb2e0efdb769b6ddb9162c6f6e55feed1
Thvich <59672915+Thvich@users.noreply.github.com>
2020-04-14 11:47:38 -0400

    Ejercicio

--- src/com/javaTest/Main.java
+++ src/com/javaTest/Main.java
@@ -3,40 +3,60 @@ package com.javaTest;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
+
 
 public class Main {
     public static void main(String[] args) {
-        doGetStringOrderAndConsonant();
+        readAlphabet();
     }
-
-    public static void doGetStringOrderAndConsonant() {
+    public static void readAlphabet(){
         char[] buff = new char[30];
-        ArrayList<Character> pivotArray = new ArrayList();
         int length;
-        try (Reader reader = Helper.openReader("file.txt");
-             Writer writer = Helper.openWriter("master.txt"))  {
+        int index = 1;
+        ArrayList<Character> Alphabet = new ArrayList<>() ;
+        try {
+            Reader reader = Files.newBufferedReader(Paths.get("file.txt"));
+            Writer writer = Files.newBufferedWriter(Paths.get("vicente.txt"));
             while((length = reader.read(buff)) >= 0) {
                 for(int i=0; i < length; i++) {
-                    pivotArray.add(buff[i]) ;
+                    Alphabet.add(buff[i]) ;
+                    System.out.println(buff[i]);
                 }
 
             }
-            //ZTROHGIDPYSAWJKCFNLMXUBEVQ
-            //[A,B,C,D,E,F,G....Z] => Helper.sortAscending(pivotArray)
-            for (Character i : Helper.sortAscending(pivotArray)) {
-                //[Z,T,R,O,H,G,I,D,..Q]
-                if (!i.toString().equals("A") && !i.toString().equals("E") && !i.toString().equals("I") && !i.toString().equals("O") && !i.toString().equals("U")) {
-                    int index =1;
-                    writer.write(i.toString(), 0, index);
-                    index ++;
-
-                }
+            Collections.sort(Alphabet);
+            System.out.print(Alphabet.toString());
+            for (Character character : Alphabet){
+                writer.write(String.valueOf(character),0,1);
             }
 
-
-        } catch(IOException e) {
-            System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
+        }catch (IOException e){
+            System.out.println(e);
         }
     }
+//    public static ArrayList<Character> sortingAlphabet(ArrayList<Character> disorderedAlphabet){
+//        ArrayList<Character> orderedAlphabet;
+//
+//        orderedAlphabet = disorderedAlphabet;
+//        return orderedAlphabet;
+//    }
+//    public static void writingAlphabet(ArrayList<Character> orderedAlphabet){
+//        //char[] buff = new char[30];
+//        Writer writer = null;
+//        //writer.write(i.toString(), 0, index);
+//        int index = 0;
+//        try {
+//
+//
+//        }catch (IOException e){
+//            System.out.println(e);
+//        }
+//    }
 }
+//for (int i = 0; i < orderedAlphabet.size(); i++)
+
+
